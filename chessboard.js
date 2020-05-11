64a98c9d68ea115bd8ce38720eba783bbea022be
Sabaliukas <63238879+Sabaliukas@users.noreply.github.com>
2020-05-11 19:41:39 +0300

    Baigta

--- chessboard.js
+++ chessboard.js
@@ -1,2 +1,40 @@
 "use strict";
 
+function renderChessboard( size ) {
+    let HTML = '';
+    const DOMchessboard = document.querySelector( '.chessboard');
+
+    //validacija
+
+    // logika
+    HTML =`
+            <div class="row" style="height: 25%;">
+                <div class="cell black" style="width: 25%;"></div>
+                <div class="cell white" style="width: 25%;"></div>
+                <div class="cell black" style="width: 25%;"></div>
+                <div class="cell white" style="width: 25%;"></div>
+            </div>
+            <div class="row" style="height: 25%;">
+                <div class="cell white" style="width: 25%;"></div>
+                <div class="cell black" style="width: 25%;"></div>
+                <div class="cell white" style="width: 25%;"></div>
+                <div class="cell black" style="width: 25%;"></div>
+            </div>
+            <div class="row" style="height: 25%;">
+                <div class="cell black" style="width: 25%;"></div>
+                <div class="cell white" style="width: 25%;"></div>
+                <div class="cell black" style="width: 25%;"></div>
+                <div class="cell white" style="width: 25%;"></div>
+            </div>
+            <div class="row" style="height: 25%;">
+                <div class="cell white" style="width: 25%;"></div>
+                <div class="cell black" style="width: 25%;"></div>
+                <div class="cell white" style="width: 25%;"></div>
+                <div class="cell black" style="width: 25%;"></div>
+        </div> `;
+    DOMchessboard.innerHTML = HTML
+    
+    return;
+}
+
+renderChessboard( 8 )
